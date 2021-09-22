baa92e0840639438e5cc3fa63f67aceaa2e5c689
Colin Ogoo <ogoo.colin@gmail.com>
2021-09-22 14:37:22 +0100

    refactor: use associated function for creating config

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,27 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In File {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
 struct Config {
-  query: String,
-  filename: String,
+    query: String,
+    filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config {query, filename}
+        Config { query, filename }
+    }
 }
