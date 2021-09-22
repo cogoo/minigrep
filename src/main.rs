162b3e018adc11d3ccac305ef8c721d279929e70
Colin Ogoo <ogoo.colin@gmail.com>
2021-09-22 19:39:12 +0100

    test: search feature

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In File {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
